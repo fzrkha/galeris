e911710a4d8c6d9c886e796d3202563a8f31ce09
fzrkha <redfanz02@gmail.com>
2023-05-24 13:24:58 +0700

    lupa ganti title

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -8,7 +8,7 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 
-    <title>Gendel Blog | {{ $title }}</title>
+    <title>Galeris | {{ $title }}</title>
   </head>
   <body>
 
