17648b90f26a6415b7d2c906c559b6986852c5b9
fzrkha <redfanz02@gmail.com>
2023-05-24 13:22:51 +0700

    bootstrap dan navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Galeris | {{ $title }}</title>
-</head>
-<body>
-    <center>Tampilan depan</center>
-</body>
-</html>
+@extends('layout.layout')
+
+@section('isi')
+    <h1>Tampilan depan.</h1>
+@endsection
---
+++ resources/views/navbar/navbar.blade.php
@@ -0,0 +1,53 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">Galeris</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+              <a class="nav-link {{ ($title === "Home") ? 'active' : '' }}" href="/">Home</a>
+          </li>
+          <!-- <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Posts
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item {{ ($title === "Contents") ? 'active' : '' }}" href="/contents">Contents</a>
+              <a class="dropdown-item {{ ($title === "Content Categories") ? 'active' : '' }}" href="/categories">Categories</a>
+            </ul>
+            </li> !-->
+          <li class="nav-item">
+              <a class="nav-link {{ ($title === "About") ? 'active' : '' }}" href="/about">About</a>
+          </li>
+  
+        </ul>
+
+        <ul class="navbar-nav">
+          @auth
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Halo, {{ auth()->user()->name }}!
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="/dashboard">My Dashboard <i class="bi bi-emoji-sunglasses"></i></a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li>
+                <form action="/logout" method="post">
+                  @csrf
+                  <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-left"></i> Logout</a></button>
+                </form>
+              </li>
+            </ul>
+          </li>
+          @else
+          <li class="nav-item">
+            <a href="/masuk" class="nav-link"><i class="bi bi-box-arrow-in-right"></i> Masuk</a>
+          </li>
+        @endauth
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
