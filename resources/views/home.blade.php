4c3e402bc8a1719fe9da79f566f573b343a5f7d8
fzrkha <redfanz02@gmail.com>
2023-05-25 14:06:56 +0700

    merubah tampilan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layout.layout')
 
 @section('isi')
-    <h1>Tampilan depan.</h1>
+        <div class="container-fluid banner">
+	        <div class="container text-center">
+		        <h4 class="display-6">Selamat datang di</h4>
+		        <h3 class="display-1">Warung Ganyu!</h3>
+		        <a href="#beranda">
+			     <button type="button" class="btn btn-danger btn-lg">
+				     Ke beranda
+			        </button>
+		        </a>
+	        </div>
+         </div>
 @endsection
