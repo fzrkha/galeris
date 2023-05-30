df06e91ad73a8eaf2732c34b8ddc6616cb11e8ca
fzrkha <redfanz02@gmail.com>
2023-05-30 10:48:13 +0700

    galeri tapi blm btul

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -4,9 +4,24 @@
     <div class="container mt-4">
         <div class="gallery-container">
             <div class="gallery">
-                <a href="#"><img src="k.png" alt=""></a>
-                <a href="#"><h3>Judul image</h3></a>
-                <p>Deskripsi...</p>
+            <div class="container text-center">
+                <div class="row pt-4">
+			        <div class="col-md-4">
+				        <img src="cosgan.jpg" width="200" height="200">
+				        <a href="#"><h3>Judul</h3></a>
+				        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur possimus amet placeat maxime saepe ipsum ratione neque eligendi! Nostrum impedit quas consequatur voluptatibus sit quos officia quaerat repellat dolor nemo!</p>
+			        </div>
+			        <div class="col-md-4">
+				        <img src="asgan.jpg" width="200" height="200">
+				        <a href="#"><h3>Judul</h3></a>
+				        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus recusandae dolor totam facere eum a architecto libero aspernatur voluptas sit! Deserunt dolorem sunt fugiat reprehenderit sint vitae minus modi id.</p>
+			        </div>
+			        <div class="col-md-4">
+				        <img src="lampnyu.jpg" width="200" height="200">
+				        <a href="#"><h3>Judul</h3></a>
+				        <p>lorem</p>
+			        </div>
+                </div>
             </div>
         </div>
     </div>
